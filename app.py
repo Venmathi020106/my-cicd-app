f5a05d0bdbeed4e71a15604bd10fc996c1083950
Venmathi V M <vmvenmathi2@gmail.com>
2026-07-30 11:38:22 +0530

    Initial commit of Python app

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+def add(a, b):
+    return a + b
+
+def test_add():
+    assert add(2, 3) == 5
+    print("Test Passed!")
+
+if __name__ == "__main__":
+    test_add()
+    print("Application is running locally!")
