99a55fe2f227549d9f82f3db8026b0539beba0eb
Venmathi V M <vmvenmathi2@gmail.com>
2026-07-30 12:19:59 +0530

    Testing failure again

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ def add(a, b):
     return a + b
 
 def test_add():
-    assert add(2, 3) == 99 # Fixed back to 5!
+    assert add(2, 3) == 5 # Fixed back to 5!
     print("Test Passed!")
 
 if __name__ == "__main__":
